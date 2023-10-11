8b503f03a9dcf5efa5ef145f726e011ce8bec213
Fred Sheppard <frederickjssheppard@gmail.com>
2023-10-11 15:23:58 +0100

    Create function to repeatedly query the user for a valid input

--- src/Main.java
+++ src/Main.java
@@ -15,4 +15,34 @@ public class Main {
             System.out.println(a);
         }
     }
+
+    /**
+     * Repeatedly prompts the user for a valid numerical input.
+     *
+     * @param input   Scanner object to receive input
+     * @param options List of valid integers
+     * @param prompts Prompts to display to the user displaying valid options
+     */
+    public static int awaitInput(Scanner input, int[] options, ArrayList<String> prompts) {
+        while (true) {
+            for (String s : prompts) {
+                System.out.println(s);
+            }
+            System.out.print("Choice: ");
+            try {
+                String choice = input.next();
+                int i = Integer.parseInt(choice);
+                // If the selection was not a valid choice
+                if (Arrays.stream(options).filter(x -> x == i).findAny().isEmpty()) continue;
+                return i;
+            } catch (NumberFormatException ignored) {
+            }
+            clearScreen();
+        }
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 }
