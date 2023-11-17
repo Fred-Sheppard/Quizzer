b0e923180c786157869e78141ac505b6a6ffad4b
Fred Sheppard <frederickjssheppard@gmail.com>
2023-11-17 17:28:29 +0000

    Remove printing logic from Quiz

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,10 +1,7 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Scanner;
+import java.util.List;
 
 /**
  * Class representing a quiz.
@@ -13,7 +10,7 @@ import java.util.Scanner;
  * This order could be random, based on difficulty,
  * or on how successfully the user has answered each question in the past.
  */
-public class Quiz {
+public abstract class Quiz {
 
     /**
      * The topic of the quiz e.g. Discrete Maths, Comp Org etc.
@@ -23,31 +20,28 @@ public class Quiz {
      * List of questions to be asked in a quiz.
      * This can be sorted to provide a unique quiz mode.
      */
-    private final ArrayList<Question> questions;
-    /**
-     * Scanner to take user input.
-     */
-    private final Scanner scanner;
+    public final ArrayList<Question> questions;
     /**
      * The user taking the quiz.
      * Quiz results will be written to this user's statistics.
      */
-    private final User user;
+    protected final User user;
+    private final UI ui;
 
     /**
      * Constructor for the Quiz class.
      *
-     * @param topic   The topic of the quiz.
-     * @param user    The user taking the quiz.
-     * @param loader  The question loader to load questions for the specified topic.
-     * @param scanner Scanner for user input.
+     * @param topic  The topic of the quiz.
+     * @param user   The user taking the quiz.
+     * @param loader The question loader to load questions for the specified topic.
+     * @param ui     Todo
      */
-    public Quiz(String topic, User user, QuestionLoader loader, Scanner scanner) {
+    public Quiz(String topic, User user, QuestionLoader loader, UI ui) {
         this.topic = topic;
         this.user = user;
+        this.ui = ui;
         // Load the questions from a file
         questions = loader.getEntries(topic);
-        this.scanner = scanner;
     }
 
     /**
@@ -57,65 +51,39 @@ public class Quiz {
      * @return If the user selected the correct answer
      */
     private boolean askQuestion(Question question) {
-        Main.clearScreen();
         // All wrong answers, plus the correct answer
-        var possibilities = question.possibilities();
+        List<String> possibilities = question.possibilities();
         // Each time a question is asked, the order should be unique
         Collections.shuffle(possibilities);
         // Find which answer (0-3) is the correct one
         // This will be used later to check if they chose correctly
         int answer = possibilities.indexOf(question.answer());
-        StringBuilder builder = new StringBuilder(question.question());
-        builder.append("\n");
-        // The number of available options for the user to select between
-        for (int i = 0; i < possibilities.size(); i++) {
-            builder.append(String.format("(%d) %s%n", i, possibilities.get(i)));
-        }
-        // Tell the user to select their answer
-        int choice = Main.promptInput(3, builder.toString(), scanner);
-        if (choice == answer) {
-            System.out.println("Correct! Well done.");
+        if (ui.askQuestion(question, answer)) {
             // If the question has never been answered by this user, it will not exist in the user history
             // If this is the case, place a zero
             user.getHistory().putIfAbsent(question.question(), 0);
             return true;
         } else {
-            System.out.println("Sorry. The correct answer was " + answer);
             // Increment the value in the map by 1
             user.getHistory().merge(question.question(), 1, Integer::sum);
             return false;
         }
     }
 
-    /**
-     * Asks all the questions in the given list.
-     * <p>
-     * This cannot be called by outside consumers,
-     * who should instead call a helper method that specifies the order to ask the questions in.
-     *
-     * @param questions The list of questions to be asked
-     */
-    private void askQuestions(ArrayList<Question> questions) {
-        Main.clearScreen();
-        int numQuestions = questions.size();
-        System.out.printf("You have selected the %s topic.%n", topic);
-        System.out.printf("This topic contains %d questions.%n", numQuestions);
-        Main.promptEnter();
+    protected int askQuestions() {
         // Ask all the questions in the sorted/shuffled list,
         // keeping track of the amount answered correctly
-        int correct = 0;
-        for (Question q : questions) {
-            boolean userCorrect = askQuestion(q);
-            if (userCorrect) correct++;
-            Main.promptEnter();
-        }
-        Main.clearScreen();
-        // Calculate the percentage of questions answered correctly
-        double percentage = (double) correct / (double) numQuestions * 100.0;
-        System.out.printf("Quiz complete! You got %d out of %d questions correct! (%.0f%%)%n",
-                correct, numQuestions, percentage);
+//        int correct = 0;
+//        for (Question q : questions) {
+//            boolean userCorrect = askQuestion(q);
+//            if (userCorrect) correct++;
+//        }
+        int correct = (int) questions.stream().filter(this::askQuestion).count();
         // Increment the number of rounds played in the user's history
         user.getHistory().merge("Rounds", 1, Integer::sum);
+        user.updateFile();
+        return correct;
+    }
 
     public String topic() {
         return topic;
@@ -173,14 +141,4 @@ class RedemptionQuiz extends Quiz {
                 .compareTo(user.getHistory().getOrDefault(a.question(), 0)));
         return super.askQuestions();
     }
-
-    /**
-     * Asks the questions in order of difficulty.
-     */
-    public void askEscalation() {
-        // Sort the questions by difficulty
-        questions.sort(Comparator.comparing(Question::difficulty));
-        askQuestions(questions);
-    }
-
 }
